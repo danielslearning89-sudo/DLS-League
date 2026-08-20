f554d6bb25c30d699e6a8470b069730e3e90b187
danielslearning89-sudo <danielslearning89@gmail.com>
2026-08-20 16:54:16 +0100

    Restore teams and matches data structure

--- data.js
+++ data.js
@@ -21,7 +21,7 @@ const matches = [
     { homeId: 6, awayId: 4, homeScore: 0, awayScore: 3, played: true, matchday: 7 },
     { homeId: 7, awayId: 3, homeScore: 3, awayScore: 0, played: true, matchday: 8 },
     { homeId: 1, awayId: 2, homeScore: 4, awayScore: 3, played: true, matchday: 9 },
-    { homeId: 5, awayId: 3, homeScore: 0, awayScore: 0, played: false, matchday: 10 },
+    { homeId: 5, awayId: 3, homeScore: 4, awayScore: 0, played: true, matchday: 10 },
     { homeId: 6, awayId: 2, homeScore: 0, awayScore: 0, played: false, matchday: 11 },
     { homeId: 7, awayId: 1, homeScore: 0, awayScore: 0, played: false, matchday: 12 },
     { homeId: 4, awayId: 2, homeScore: 0, awayScore: 0, played: false, matchday: 13 },
